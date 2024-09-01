e06a3726ae0161763c302beb9d8a00e0358785bc
aras370 <hesamehsani72@yahoo.com>
2024-09-01 23:38:11 +0330

    commit infra

---
+++ InfraStructure/Senders/SendMail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraStructure.Senders
+{
+    public static class SendMail
+    {
+
+        public static void Send(string to, string subject, string body)
+        {
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("hesamehsani211@gmail.com", "Dating App!");
+            mail.To.Add(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+
+
+            SmtpClient SmtpServer = new SmtpClient();
+            SmtpServer.Host = "smtp.gmail.com";
+            SmtpServer.Port = 587;
+
+            SmtpServer.Credentials = new System.Net.NetworkCredential("hesamehsani211@gmail.com", "sdpbtaysactqipnr");
+            SmtpServer.EnableSsl = true;
+            SmtpServer.Send(mail);
+
+
+        }
+
+    }
+}
